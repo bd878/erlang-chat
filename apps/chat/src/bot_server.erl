16e30b4b437298f028df1e0d421925131d9a50e4
bd878 <slavad878@gmail.com>
2025-08-26 21:24:23 +0300

    add types

--- apps/chat/src/bot_server.erl
+++ apps/chat/src/bot_server.erl
@@ -2,17 +2,27 @@
 
 -behaviour(gen_server).
 
--export([init/1, stop/0, start_link/0, handle_cast/2, handle_call/3, terminate/2]).
+-export([init/1]).
+-export([stop/0]).
+-export([start_link/0]).
+-export([handle_cast/2]).
+-export([handle_call/3]).
+-export([terminate/2]).
 -export([loop/0]).
+
 -define(SERVER, ?MODULE).
 
+%%% CALLBACKS
+-spec start_link() -> {ok, pid()} | {error, term()}.
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?SERVER, [], []).
 
+-spec init(term()) -> {ok, {}}.
 init(_) ->
     spawn_link(?SERVER, loop, []),
     {ok, {}}.
 
+-spec loop() -> no_return().
 loop() ->
     timer:send_after(rand:uniform(10000), self(), {message, gen_random_string()}),
     receive
@@ -29,7 +39,8 @@ handle_call(_, _, _) -> ok.
 
 terminate(_, _) -> ok.
 
-stop() ->
-    gen_server:cast(?SERVER, stop).
+stop() -> gen_server:cast(?SERVER, stop).
 
+%%% PRIVATE
+-spec gen_random_string() -> binary().
 gen_random_string() -> <<"test">>.
